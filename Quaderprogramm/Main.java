56870b6a6e2b0875ba20035cb32113cb9134fe71
11sloe <43333134+11sloe@users.noreply.github.com>
2022-08-15 08:45:38 +0200

    Add files via upload

---
+++ Quaderprogramm/Main.java
@@ -0,0 +1,23 @@
+
+/**
+ * Beispielprogramm
+ * 
+ * @author sloe
+ * @version 15.08.22
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int laenge = 5;
+        int breite = 4;
+        int hoehe = 2;
+        
+        System.out.println("Mein Quader: ");
+        System.out.println("Laenge: " + laenge);
+        System.out.println("Breite: " + breite);
+        System.out.println("Hoehe: " + hoehe);
+        
+        System.out.println("Volumen: " + laenge * breite * hoehe);
+    }
+}
